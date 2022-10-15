b6643aa678b541c7569d4d4623679c5dc97d0531
JKincorperated <G.T.H.T.I.A.T.A@gmail.com>
2022-10-15 18:44:03 +0100

    update

--- controller.lua
+++ controller.lua
@@ -1,12 +1,33 @@
 rednet.open("top")
 os.loadAPI("json")
 
+local vericode = require("vericode")
+
+local key = vericode.loadKey("/keys/key.key")
+
 function Update()
-    local newest_controler = http.get("https://raw.githubusercontent.com/JKincorperated/Tekkit2-Computercraft-Space-Station/main/controller.lua").readAll()
+    local newest_controller = http.get("https://raw.githubusercontent.com/JKincorperated/Tekkit2-Computercraft-Space-Station/main/controller.lua").readAll()
     local updates = http.get("https://raw.githubusercontent.com/JKincorperated/Tekkit2-Computercraft-Space-Station/main/updates.json").readAll()
     updates = json.decode(updates)
-    for index, value in ipairs(updates) do
-        print(index, value)
+    for _, value in ipairs(updates) do
+        vericode.send(value["id"], "UPDATE-PENDING", key, "updates")
+        --value["link"]
+    end
+    sleep(60)
+    for _, value in ipairs(updates) do
+        vericode.send(value["id"], "UPDATE-START", key, "updates")
+        --value["link"]
+    end
+    for _, value in ipairs(updates) do
+        vericode.send(value["id"], "UPDATE-DOWNLOAD", key, "updates")
+        
+        local patch = http.get(value["link"]).readAll()
+
+        vericode.send(value["id"], patch, key, "updates")
+        
+        vericode.send(value["id"], "UPDATE-RESTART", key, "updates")
+
+        os.reboot()
     end
 end
 
