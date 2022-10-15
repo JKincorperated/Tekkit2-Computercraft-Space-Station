7be3e812ebd223bd3b6cb3db9b5d470db6c9f0b2
JKincorperated <G.T.H.T.I.A.T.A@gmail.com>
2022-10-15 16:32:34 +0100

    update

--- controller.lua
+++ controller.lua
@@ -2,10 +2,10 @@ rednet.open("top")
 
 function Update()
     local newest_controler = http.get("https://raw.githubusercontent.com/JKincorperated/Tekkit2-Computercraft-Space-Station/main/controller.lua").readAll()
-    local index = http.get("https://raw.githubusercontent.com/JKincorperated/Tekkit2-Computercraft-Space-Station/main/index.lua").readAll()
-    fs.open("index.lua", "w").write(index)
-    local index = require("index")
-    local updates = index.updates
+    local indexs = http.get("https://raw.githubusercontent.com/JKincorperated/Tekkit2-Computercraft-Space-Station/main/index.lua").readAll()
+    fs.open("index.lua", "w").write(indexs)
+    local indexs2 = os.loadAPI("index")
+    local updates = indexs2.updates
     print(updates)
 end
 
