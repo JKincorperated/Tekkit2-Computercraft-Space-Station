0e7bb99f4b995b9d250c4e9f67301a97c41bfc7c
JKincorperated <G.T.H.T.I.A.T.A@gmail.com>
2022-10-16 17:43:48 +0100

    update

--- 319.lua
+++ 319.lua
@@ -1,9 +1,24 @@
 rednet.open("top")
 
+local args = {...}
 
-
-while true do
-    local message = rednet.receive("updates")
-    print(message)
+if args[1] == "code" then
+    while true do
+        print("Hello World!")
+        sleep(1)
+    end
     
+else
+    if fs.open("prime", "r").read() ~= "true" then
+        shell.openTab("319.lua", "code")
+    end
+    
+    while true do
+        local message = rednet.receive("updates")
+        if message == "UPDATE-PRIME" then
+            fs.open("prime", "w").write("true")
+            os.reboot()
+        end
+        
+    end
 end
