968e61c36e4c7681e8117a49456c3329eee725ed
JKincorperated <G.T.H.T.I.A.T.A@gmail.com>
2022-10-16 17:23:42 +0100

    update

--- 319.lua
+++ 319.lua
@@ -1,23 +1,9 @@
 rednet.open("top")
 
-Freeze = false
 
-local program = coroutine.create(function ()
-    while true do
-        if Freeze then
-            coroutine.yield()
-        end
-        print("Hi")
-        sleep(1)
-    end
-end)
-
-	
-coroutine.resume(program)
-sleep(10)
-Freeze = true
 
 while true do
     local message = rednet.receive("updates")
+    print(message)
     
 end
