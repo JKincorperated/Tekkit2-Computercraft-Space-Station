cd2ae06a0cca1d42106feb62c3f80bcf866d88dd
JKincorperated <G.T.H.T.I.A.T.A@gmail.com>
2022-10-17 16:47:33 +0100

    QOL fixes

--- 341.lua
+++ 341.lua
@@ -1,12 +1,12 @@
 rednet.open("top")
 
-local args = {...}
+local args = { ... }
 
 local id = os.getComputerID()
 
 redstone.setOutput("back", false)
 
-term.setCursorPos(0,0)
+term.setCursorPos(0, 0)
 
 print(" --- System Online --- ")
 
@@ -14,46 +14,52 @@ print(" --- System Online --- ")
 --if args[1] == "code" then
 --    while true do
 --        local _, message = rednet.receive()
---        
+--
 --    end
---    
+--
 --else
-    while true do
-        local _, message = rednet.receive()
-        if message == "UPDATE-PRIME" then
-            fs.open("prime", "w").write("true")
-            os.reboot()
-        end
-        if message == "LIGHTS-ON" then
-            redstone.setOutput("back", true)
-        end
-        if message == "LIGHTS-OFF" then
-            redstone.setOutput("back", false)
-        end
-        if message == "UPDATE-STARTUP" then
-            print("Updating Startup Files...")
-            local _, patch = rednet.receive()
-            fs.delete("startup.lua")
-            fs.open("startup.lua", "w").write(patch)
-            print("Success")
-        end
+if fs.open("prime", "r") == nil or fs.open("prime", "r").read(5) ~= "true" then
+    redstone.setOutput("bottom", false)
+else
+    print(" ---- Computer " .. id .. " Primed For Update ---- ")
+end
 
-        if message == "UPDATE-DOWNLOAD" then
-            print("Updating System Files...")
-            local _, patch = rednet.receive()
-            fs.delete(id .. ".lua")
-            fs.open(id .. ".lua", "w").write(patch)
-            print("Success")
-        end
-
-        if message == "UPDATE-COMPLETE" then
-            print("Update Complete")
-        end
-
-        if message == "UPDATE-RESTART" then
-            print("Rebooting...")
-            fs.open("prime", "w").write()
-            os.reboot()
-        end
+while true do
+    local _, message = rednet.receive()
+    if message == "UPDATE-PRIME" then
+        fs.open("prime", "w").write("true")
+        os.reboot()
     end
+    if message == "LIGHTS-ON" then
+        redstone.setOutput("back", true)
+    end
+    if message == "LIGHTS-OFF" then
+        redstone.setOutput("back", false)
+    end
+    if message == "UPDATE-STARTUP" then
+        print("Updating Startup Files...")
+        local _, patch = rednet.receive()
+        fs.delete("startup.lua")
+        fs.open("startup.lua", "w").write(patch)
+        print("Success")
+    end
+
+    if message == "UPDATE-DOWNLOAD" then
+        print("Updating System Files...")
+        local _, patch = rednet.receive()
+        fs.delete(id .. ".lua")
+        fs.open(id .. ".lua", "w").write(patch)
+        print("Success")
+    end
+
+    if message == "UPDATE-COMPLETE" then
+        print("Update Complete")
+    end
+
+    if message == "UPDATE-RESTART" then
+        print("Rebooting...")
+        fs.open("prime", "w").write()
+        os.reboot()
+    end
+end
 --end
