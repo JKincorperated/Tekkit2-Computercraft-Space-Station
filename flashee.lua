d8aae2526a054874208429a0c8fb0bb14a322b4a
JKincorperated <G.T.H.T.I.A.T.A@gmail.com>
2022-10-17 11:16:58 +0100

    fix no monitor

--- flashee.lua
+++ flashee.lua
@@ -9,33 +9,23 @@ while true do
         os.reboot()
     end
     if message == "UPDATE-STARTUP" then
-        mon.setCursorPos(1,15)
-        mon.write("      Upgrading BIOS      ")
         print("Updating Startup Files...")
         local _, patch = rednet.receive()
         fs.delete("startup.lua")
         fs.open("startup.lua", "w").write(patch)
         print("Success")
-        mon.setCursorPos(1,15)
-        mon.write("      Upgraded BIOS      ")
     end
 
     if message == "UPDATE-DOWNLOAD" then
-        mon.setCursorPos(1,15)
-        mon.write("     Upgrading System     ")
         print("Updating System Files...")
         local _, patch = rednet.receive()
         fs.delete(id .. ".lua")
         fs.open(id .. ".lua", "w").write(patch)
         print("Success")
-        mon.setCursorPos(1,15)
-        mon.write("     Upgraded System     ")
     end
 
     if message == "UPDATE-COMPLETE" then
         print("Update Complete")
-        mon.setCursorPos(1,15)
-        mon.write("     Upgrade Complete     ")
     end
 
     if message == "UPDATE-RESTART" then
